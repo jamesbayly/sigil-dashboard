0eaccfa3621819cde132fcaa7356f28d8177eb13
James Bayly <james@bayly.xyz>
2025-06-13 10:59:09 +0800

    Update hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { StrictMode } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
       <Toaster />
     </ThemeProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
