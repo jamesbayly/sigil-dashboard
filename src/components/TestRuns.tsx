cf1e0ae53354855a5f132a4d3cc0944c482bd819
James Bayly <james@bayly.xyz>
2026-03-15 08:06:37 +1300

    Show ID

--- src/components/TestRuns.tsx
+++ src/components/TestRuns.tsx
@@ -19,6 +19,20 @@ export default function TestRunsView() {
 
   const columns: ColumnDef<StrategyTestRunsResponse>[] = [
     {
+      accessorKey: "id",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            ID
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    {
       accessorKey: "name",
       header: ({ column }) => {
         return (
